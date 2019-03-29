e7a1b496e362140cf40ca2a3a7ecc53c0061c2fd
venrique <00046517@uca.edu.sv>
2019-03-28 18:01:33 -0600

    Se agregaron imagenes y mejoras del diseño

--- app/src/main/java/com/venrique/pokedex/PokemonInfo.kt
+++ app/src/main/java/com/venrique/pokedex/PokemonInfo.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.venrique.pokedex.utils.NetworkUtils
 import org.json.JSONObject
 import org.w3c.dom.Text
@@ -17,6 +19,7 @@ class PokemonInfo : AppCompatActivity() {
     var infoPokePeso: TextView?= null
     var infoPokeAltura: TextView?= null
     var infoPokeTipo: TextView?=null
+    var infoImagen: ImageView?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +30,7 @@ class PokemonInfo : AppCompatActivity() {
         infoPokePeso = findViewById(R.id.peso)
         infoPokeAltura = findViewById(R.id.altura)
         infoPokeTipo = findViewById(R.id.tipos)
+        infoImagen = findViewById(R.id.imagen)
 
         val intent: Intent = getIntent()
 
@@ -63,17 +67,24 @@ class PokemonInfo : AppCompatActivity() {
                 var pokeArray = jsonPokemons.getJSONArray("abilities")
                 var pokeType = jsonPokemons.getJSONArray("types")
 
+
                 var listaP = mutableListOf<String>()
 
                 var listaT = mutableListOf<String>()
+
+                infoImagen?.let {
+                    Glide.with(this@PokemonInfo).load(jsonPokemons.getJSONObject("sprites").getString("front_default")).into(
+                        it
+                    )
+                }
 
                 for (i in 0..pokeArray.length()-1) listaP.add(i,pokeArray.getJSONObject(i).getJSONObject("ability").getString("name"))
 
                 for(e in 0..pokeType.length()-1) listaT.add(e,pokeType.getJSONObject(e).getJSONObject("type").getString("name"))
 
                 infoPokeInfo?.setText(listaP.toString())
-                infoPokePeso?.setText(jsonPokemons.getString("weight")+" hectogramos")
-                infoPokeAltura?.setText(jsonPokemons.getString("height")+" decímetros")
+                infoPokePeso?.setText((jsonPokemons.getInt("weight")/10).toString() + " Kg")
+                infoPokeAltura?.setText((jsonPokemons.getInt("height")/10).toString() +" m")
                 infoPokeTipo?.setText(listaT.toString())
                 //pokeArray.getJSONObject(0).getJSONObject("pokemon").getString("name")
             } else {
